5a134dadebf94e49b393610297f1590f694baf7b
Gleb <glebpetrof2007@gmail.com>
2024-12-05 13:26:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import random
-a = [[random.randint(-10, 10) for i in range(1,5)] for i in range(1,21)]
-unique=[]
-for pair in a:
+pairs_list = [[random.randint(-10, 10) for i in range(1,5)] for i in range(1,21)]
+unique = []
+for pair in pairs_list:
     if pair not in unique:
         unique.append(tuple(pair))
 print(unique)
-n=int(input("Vvedi chislo"))
-count=0
-sum=0
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        sum+=a[i][j]
-    if(sum<n):
-        count+=1
+n = int(input("Vvedi chislo"))
+count = 0
+sum = 0
+for i in range(len(pairs_list)):
+    for j in range(len(pairs_list[i])):
+        sum += pairs_list[i][j]
+    if sum < n:
+        count += 1
 print(count)
